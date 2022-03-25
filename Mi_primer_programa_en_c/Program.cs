d437955600e0814d0feb0d7e72644918a389b73b
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 03:30:01 -0700

    Agregar archivos de proyecto.

---
+++ Mi_primer_programa_en_c/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mi_primer_programa_en_c
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Mi primer programa en C#");
+        }
+    }
+}
